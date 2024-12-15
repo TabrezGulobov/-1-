8bb340ea6c02421a2b44b026c6da3d37ec1b06fa
TabrezGulobov <tabrezgulobov@gmail.com>
2024-12-15 17:23:13 +0500

    Add files via upload

---
+++ ConsoleApp61/ConsoleApp61/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp7
+
+{
+    class Pogram
+    {
+        static void A(int a)
+        {
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("Четное");
+            }
+            else
+            {
+                Console.WriteLine("Нечётьное");
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+            A(3);
+            A(4);
+            A(5);
+            A(6);
+            A(7);
+        }
+    }
+}
